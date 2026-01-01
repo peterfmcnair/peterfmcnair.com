f4efca3605b1263a8d8699750e515f9f9ff82fbb
peterfmcnair <81016541+peterfmcnair@users.noreply.github.com>
2026-01-01 16:04:08 +1100

    feat: add dynamic wants list

---
+++ scripts/fetch_todoist.rb
@@ -0,0 +1,32 @@
+require "net/http"
+require "json"
+require "yaml"
+require "uri"
+
+TOKEN = ENV["TODOIST_TOKEN"]
+PROJECT_ID = ENV["TODOIST_PROJECT_ID"]
+SECTION_ID = ENV["TODOIST_SECTION_ID"]
+
+abort "Missing TODOIST_TOKEN" unless TOKEN
+abort "Missing TODOIST_PROJECT_ID" unless PROJECT_ID
+abort "Missing TODOIST_SECTION_ID" unless SECTION_ID
+
+uri = URI("https://api.todoist.com/rest/v2/tasks?project_id=#{PROJECT_ID}&section_id=#{SECTION_ID}")
+
+req = Net::HTTP::Get.new(uri)
+req["Authorization"] = "Bearer #{TOKEN}"
+
+res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+  http.request(req)
+end
+
+abort "Todoist API error: #{res.code}" unless res.code == "200"
+
+tasks = JSON.parse(res.body)
+
+# Sort by due date then name
+tasks.sort_by! { |t| [t.dig("due", "date") || "9999-12-31", t["content"]] }
+
+File.write("_data/todoist.yml", tasks.to_yaml)
+
+puts "Fetched #{tasks.length} tasks"
