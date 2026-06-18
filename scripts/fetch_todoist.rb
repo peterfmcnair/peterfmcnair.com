aebe47a3cdb02159c75646034c1f20cf8591b98b
peterfmcnair <81016541+peterfmcnair@users.noreply.github.com>
2026-06-18 21:54:51 +1000

    fix: created_at became added_at

--- scripts/fetch_todoist.rb
+++ scripts/fetch_todoist.rb
@@ -25,7 +25,7 @@ abort "Todoist API error: #{res.code}" unless res.code == "200"
 tasks = JSON.parse(res.body)
 
 # Sort by created_at (newest first)
-tasks.sort_by { |t| -t["created_at"].to_i }
+tasks.sort_by { |t| -t["added_at"].to_i }
 
 
 File.write("_data/todoist.yml", tasks.to_yaml)
